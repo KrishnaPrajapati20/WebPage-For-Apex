bbb001412f12eb972efd593b7c04216735c9723a
KrishnaKumar <bcaplain2023@gmail.com>
2025-07-29 01:51:09 +0530

    Add files via upload

---
+++ Apex.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const button = document.getElementById("alertBtn");
+    button.addEventListener("click", () => {
+        alert("Hello! You clicked the premium button successfully.");
+    });
+});
